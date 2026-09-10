64d6e74e289fc68d133d5c27778dbac156882c20
kalpesh <kalpesh162@gmail.com>
2026-09-10 15:28:01 +0530

    Http Sesion -1

---
+++ SessionDemo-01/src/main/java/p1/SessionServlet.java
@@ -0,0 +1,40 @@
+package p1;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/SessionServlet")
+public class SessionServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		
+		// How to get Sesssion Object 
+		
+		HttpSession session=request.getSession();
+		
+		out.print("<h1>");
+		
+		out.print("Session ID"+session.getId() +" <br>");
+		out.print("Session CreationTime  "+session.getCreationTime() +" <br>");
+		out.print("Session CreationTime  "+new Date(session.getCreationTime()) +" <br>");
+		out.print("Session LastAccesssTime  "+session.getLastAccessedTime() +" <br>");
+		out.print("Session LastAccesssTime  "+new Date(session.getLastAccessedTime()) +" <br>");
+		out.print("Session Interval  "+session.getMaxInactiveInterval());
+		
+		out.print("</h1>");
+		
+	
+	}
+
+}
