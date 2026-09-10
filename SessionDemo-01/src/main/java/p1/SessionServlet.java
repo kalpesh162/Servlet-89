7c0b7cf6bf6b15e630f7dffafc9de2c1602dfa5e
kalpesh <kalpesh162@gmail.com>
2026-09-10 16:15:29 +0530

    Http Sesion -2

--- SessionDemo-01/src/main/java/p1/SessionServlet.java
+++ SessionDemo-01/src/main/java/p1/SessionServlet.java
@@ -14,27 +14,41 @@ import javax.servlet.http.HttpSession;
 @WebServlet("/SessionServlet")
 public class SessionServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
 		response.setContentType("text/html");
-		PrintWriter out=response.getWriter();
-		
-		// How to get Sesssion Object 
-		
-		HttpSession session=request.getSession();
-		
+		PrintWriter out = response.getWriter();
+
+		// How to get Sesssion Object
+
+		HttpSession session = request.getSession();
+		Integer cnt = 1;
+
+		if (session.getAttribute("cnt") != null) {
+
+			cnt = (Integer) session.getAttribute("cnt") + 1;
+			session.setAttribute("cnt", cnt);
+
+		} else {
+			session.setAttribute("cnt", 1);
+			session.setMaxInactiveInterval(120);
+		}
+
 		out.print("<h1>");
-		
-		out.print("Session ID"+session.getId() +" <br>");
-		out.print("Session CreationTime  "+session.getCreationTime() +" <br>");
-		out.print("Session CreationTime  "+new Date(session.getCreationTime()) +" <br>");
-		out.print("Session LastAccesssTime  "+session.getLastAccessedTime() +" <br>");
-		out.print("Session LastAccesssTime  "+new Date(session.getLastAccessedTime()) +" <br>");
-		out.print("Session Interval  "+session.getMaxInactiveInterval());
-		
+
+		out.print("VISIT COUNT  " + cnt + "<br>");
+
+		out.print("Session ID" + session.getId() + " <br>");
+		out.print("Session CreationTime  " + session.getCreationTime() + " <br>");
+		out.print("Session CreationTime  " + new Date(session.getCreationTime()) + " <br>");
+		out.print("Session LastAccesssTime  " + session.getLastAccessedTime() + " <br>");
+		out.print("Session LastAccesssTime  " + new Date(session.getLastAccessedTime()) + " <br>");
+		out.print("Session Interval  " + session.getMaxInactiveInterval());
+
 		out.print("</h1>");
-		
-	
+
 	}
 
 }
